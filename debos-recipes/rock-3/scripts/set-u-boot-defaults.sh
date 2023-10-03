fa2a0bf87ce11c9744e63620f1f46c1999eb49dd
Aaron Dewes <aaron.dewes@protonmail.com>
2023-10-03 20:00:05 +0200

    CMRat

--- debos-recipes/rock-3/scripts/set-u-boot-defaults.sh
+++ debos-recipes/rock-3/scripts/set-u-boot-defaults.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 BOARD="$1"
 if [ "$BOARD" = "rock3a" ] ; then
-    FDT_NAME="rk3568-rock-3a.dtb"
-    FDT_OVERLAYS=""
+    FDT_NAME="rockchip/rk3568-rock-3a.dtb"
+    FDT_DIR="/usr/lib/linux-image-"
 elif [ "$BOARD" = "bitpirat" ] ; then
-    FDT_NAME="rk3566-radxa-cm3-io.dtb"
-    FDT_OVERLAYS="bitpirat.dtbo"
+    FDT_NAME="cmrat.dtb"
+    FDT_DIR="/boot/dtb"
 fi
 
 mkdir -p /usr/share/u-boot-menu/conf.d/
@@ -16,8 +16,5 @@ U_BOOT_MENU_LABEL="Citadel OS"
 U_BOOT_PARAMETERS="ignore_loglevel ro rootwait earlycon console=tty0 console=ttyS2,1500000n8 fw_devlink=off"
 U_BOOT_ROOT="root=LABEL=root"
 U_BOOT_TIMEOUT="30"
-U_BOOT_FDT="rockchip/$FDT_NAME"
-U_BOOT_FDT_OVERLAYS="$FDT_OVERLAYS"
-U_BOOT_FDT_OVERLAYS_DIR="/boot/dtbo/"
-
+U_BOOT_FDT="$FDT_NAME"
 EOF
