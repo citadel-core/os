38ed35b5d5bcc03484fb531279b24584ac6efb66
Aaron Dewes <aaron.dewes@protonmail.com>
2023-10-03 10:49:04 +0200

    Fix Radxa config

--- debos-recipes/radxa/scripts/set-u-boot-defaults.sh
+++ debos-recipes/radxa/scripts/set-u-boot-defaults.sh
@@ -3,9 +3,9 @@ BOARD="$1"
 FDT_OVERLAYS=""
 if [ "$BOARD" = "rock3a" ] ; then
     FDT_NAME="rk3568-rock-3a.dtb"
-elif [ "$BOARD" = "cm3io" ] ; then
-    FDT_NAME="rk3566-radxa-cm3-io.dtb"
-    FDT_OVERLAYS="radxa-cm3-io-pcie2x1.dtbo"
+elif [ "$BOARD" = "cm3raspcm4io" ] ; then
+    FDT_NAME="rk3566-radxa-cm3-rpi-cm4-io.dtb"
+    FDT_OVERLAYS="radxa-cm3-raspcm4io-pcie2x1.dtbo"
 elif [ "$BOARD" = "cm5io" ] ; then
     FDT_NAME="rk3588s-radxa-cm5-io.dtb"
 fi
@@ -19,16 +19,12 @@ cat <<EOF > /usr/share/u-boot-menu/conf.d/10-citadel-os.conf
 
 #U_BOOT_UPDATE="true"
 
-#U_BOOT_ALTERNATIVES="default recovery"
-#U_BOOT_DEFAULT="l0"
-#U_BOOT_ENTRIES="all"
-#U_BOOT_MENU_LABEL="Citadel OS"
+U_BOOT_MENU_LABEL="Citadel OS"
 U_BOOT_PARAMETERS="ignore_loglevel ro rootwait earlycon console=tty0 console=ttyS2,1500000n8 fw_devlink=off"
 U_BOOT_ROOT="root=LABEL=root"
 U_BOOT_TIMEOUT="30"
 U_BOOT_FDT="rockchip/$FDT_NAME"
-#U_BOOT_FDT_DIR="/usr/lib/linux-image-"
 U_BOOT_FDT_OVERLAYS="${FDT_OVERLAYS}"
-U_BOOT_FDT_OVERLAYS_DIR="/boot/dtbo/"
+U_BOOT_FDT_OVERLAYS_DIR="/usr/lib/linux-image-"
 
 EOF
